f2eb67706cb8c3acdbedf6ebd50f27543bd4e3ea
Tonmuy Ahamed Sifat <tonmoysifatmd@gmail.com>
2026-03-18 02:26:18 +0600

    added title in the subject

--- src/pages/subjects/list.tsx
+++ src/pages/subjects/list.tsx
@@ -1,8 +1,15 @@
-import React from 'react'
+import {ListView} from "@/components/refine-ui/views/list-view.tsx";
+import {Breadcrumb} from "@/components/refine-ui/layout/breadcrumb.tsx";
 
 const SubjectsList = () => {
   return (
-    <div>List</div>
+    <ListView>
+      <Breadcrumb/>
+      <h1 className="page-title">Subjects</h1>
+      <div className="intro-row">
+        <p>Quick access to essential metrics and management tools.</p>
+      </div>
+    </ListView>
   )
 }
 export default SubjectsList
