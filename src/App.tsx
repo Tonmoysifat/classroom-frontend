0800e7dfe12efaa12c2654c9a3cc0ba7c15644ea
Tonmuy Ahamed Sifat <tonmoysifatmd@gmail.com>
2026-03-18 00:52:55 +0600

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,26 @@
-import { GitHubBanner, Refine, WelcomePage } from "@refinedev/core";
-import { DevtoolsPanel, DevtoolsProvider } from "@refinedev/devtools";
-import { RefineKbar, RefineKbarProvider } from "@refinedev/kbar";
+import {Refine} from "@refinedev/core";
+import {DevtoolsPanel, DevtoolsProvider} from "@refinedev/devtools";
+import {RefineKbar, RefineKbarProvider} from "@refinedev/kbar";
 
 import routerProvider, {
   DocumentTitleHandler,
   UnsavedChangesNotifier,
 } from "@refinedev/react-router";
-import { BrowserRouter, Route, Routes } from "react-router";
+import {BrowserRouter, Outlet, Route, Routes} from "react-router";
 import "./App.css";
-import { Toaster } from "./components/refine-ui/notification/toaster";
-import { useNotificationProvider } from "./components/refine-ui/notification/use-notification-provider";
-import { ThemeProvider } from "./components/refine-ui/theme/theme-provider";
-import { dataProvider } from "./providers/data";
+import {Toaster} from "./components/refine-ui/notification/toaster";
+import {useNotificationProvider} from "./components/refine-ui/notification/use-notification-provider";
+import {ThemeProvider} from "./components/refine-ui/theme/theme-provider";
+import {dataProvider} from "./providers/data";
+import Dashboard from "@/pages/dashboard.tsx";
+import {BookOpen, Home} from "lucide-react";
+import {Layout} from "@/components/refine-ui/layout/layout.tsx";
+import SubjectsList from "@/pages/subjects/list.tsx";
+import SubjectsCreate from "@/pages/subjects/create.tsx";
 
 function App() {
   return (
     <BrowserRouter>
-      <GitHubBanner />
       <RefineKbarProvider>
         <ThemeProvider>
           <DevtoolsProvider>
@@ -29,16 +33,45 @@ function App() {
                 warnWhenUnsavedChanges: true,
                 projectId: "APokHV-TmVtXJ-LraASL",
               }}
+
+              resources={[
+                {
+                  name: "dashboard",
+                  list: "/",
+                  meta: {label: "Home", icon: <Home/>}
+                },
+                {
+                  name: "subjects",
+                  list: "/subjects",
+                  create: "/subjects/create",
+                  meta: {label: "Subjects", icon: <BookOpen/>}
+                }
+              ]}
             >
               <Routes>
-                <Route index element={<WelcomePage />} />
+                <Route element={
+                  <Layout>
+                    <Outlet/>
+                  </Layout>
+                }
+
+                >
+                  <Route path="/" element={<Dashboard/>}/>
+
+                  <Route path="subjects">
+                    <Route index element={<SubjectsList/>}/>
+                    <Route path="create" element={<SubjectsCreate/>}/>
+
+                  </Route>
+
+                </Route>
               </Routes>
-              <Toaster />
-              <RefineKbar />
-              <UnsavedChangesNotifier />
-              <DocumentTitleHandler />
+              <Toaster/>
+              <RefineKbar/>
+              <UnsavedChangesNotifier/>
+              <DocumentTitleHandler/>
             </Refine>
-            <DevtoolsPanel />
+            <DevtoolsPanel/>
           </DevtoolsProvider>
         </ThemeProvider>
       </RefineKbarProvider>
